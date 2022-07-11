8923cabfb704fefbbbbff44926608677b139be74
Edward Abboud <abboudedward@gmail.com>
2022-07-11 19:23:07 +0200

    started working on homepage

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,12 +1,21 @@
 import React from "react";
-
 import TEST_ID from "./Home.testid";
+import style from "./Home.module.css";
+import logo from "../../assets/droppy-logo.svg";
 
 const Home = () => {
   return (
     <div data-testid={TEST_ID.container}>
-      <h1>This is the homepage</h1>
-      <p>Good luck with the project!</p>
+      <div className={style.homePage}>
+        <div className={style.logo}>
+          <img src={logo} />
+        </div>
+        <button className={style.button}>GET STARTED</button>
+        <p className={style.statement - "body-desktop"}>
+          {"You don't need a car to carry your stuff,<br></br>You need Droppy!"}
+        </p>
+        <div>Already have an account? Log in here</div>
+      </div>
     </div>
   );
 };
