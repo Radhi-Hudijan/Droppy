a33a46bc9c684f9b2e26cdd484520c7b64844f29
Suleyman Yilmaz <yilmazsgit@gmail.com>
2022-07-13 22:49:24 +0200

    Sign up page and Add car page routes are added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import Home from "./pages/Home/Home";
-import CreateUser from "./pages/User/CreateUser";
+import AddCar from "./pages/Signup/AddCar";
+import Signup from "./pages/Signup/Signup";
+// import CreateUser from "./pages/User/CreateUser";
 import UserList from "./pages/User/UserList";
 
 const App = () => {
@@ -12,7 +14,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/user" element={<UserList />} />
-        <Route path="/user/create" element={<CreateUser />} />
+        <Route path="/user/create" element={<Signup />}></Route>
+        <Route path="/user/create/add-car" element={<AddCar />} />
       </Routes>
     </>
   );
