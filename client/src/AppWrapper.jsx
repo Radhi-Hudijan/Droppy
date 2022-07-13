2fb2d7af9efcc0c99afb0a7390a8d5a75c4c219f
Suleyman Yilmaz <yilmazsgit@gmail.com>
2022-07-13 22:53:36 +0200

    UserInfoContextProvider is added

--- client/src/AppWrapper.jsx
+++ client/src/AppWrapper.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { BrowserRouter as Router } from "react-router-dom";
+import { UserInfoContextProvider } from "./context/UserInfoContext";
 
 /**
  * This component wraps our App with the providers we do not want to have in our tests
  */
 const AppWrapper = ({ children }) => {
-  return <Router>{children}</Router>;
+  return (
+    <UserInfoContextProvider>
+      <Router>{children}</Router>
+    </UserInfoContextProvider>
+  );
 };
 
 AppWrapper.propTypes = {
