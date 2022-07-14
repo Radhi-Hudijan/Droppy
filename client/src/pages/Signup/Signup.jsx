ffbae62089173a84e49e060782a71ba5d8933840
Suleyman Yilmaz <yilmazsgit@gmail.com>
2022-07-14 10:45:18 +0200

    SIgn up page is created

---
+++ client/src/pages/Signup/Signup.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import NewUserForm from "./NewUserForm";
+import useFetch from "../../hooks/useFetch";
+import UserInfoContext from "../../context/UserInfoContext";
+
+function Signup() {
+  const navigate = useNavigate();
+  const [isClicked, setIsClicked] = useState(false);
+  const { setEmail, setName, setSurname } = useContext(UserInfoContext);
+  const userDataOnSuccess = {
+    email: "",
+    name: "",
+    surname: "",
+  };
+
+  const onSuccess = () => {
+    setName(userDataOnSuccess.name);
+    setSurname(userDataOnSuccess.surname);
+    setEmail(userDataOnSuccess.email);
+    navigate(isClicked ? "/user/create/add-car" : "/", {
+      replace: true,
+    });
+  };
+
+  const { isLoading, error, performFetch, cancelFetch } = useFetch(
+    "/user/create",
+    onSuccess
+  );
+  useEffect(() => {
+    return cancelFetch;
+  }, []);
+
+  function addUserHandler(user) {
+    userDataOnSuccess.name = user.name;
+    userDataOnSuccess.surname = user.surname;
+    userDataOnSuccess.email = user.email;
+    performFetch({
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+  }
+
+  let statusComponent = null;
+  if (error != null) {
+    statusComponent = (
+      <div>Error while trying to create user: {error.toString()}</div>
+    );
+  } else if (isLoading) {
+    statusComponent = <div>Creating user....</div>;
+  }
+
+  return (
+    <section data-testid="container">
+      <h1 className="h1-desktop">Sign up</h1>
+      <NewUserForm onAddUser={addUserHandler} setIsClicked={setIsClicked} />
+      {statusComponent}
+    </section>
+  );
+}
+
+export default Signup;
