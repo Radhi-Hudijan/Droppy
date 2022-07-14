3ff80b000ced5fbcc480dce0fefc2210b4f115c8
Edward Abboud <97112418+EdwardAbboud@users.noreply.github.com>
2022-07-14 12:02:50 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import Home from "./pages/Home/Home";
-
 import CreateUser from "./pages/User/CreateUser";
 import UserList from "./pages/User/UserList";
 
