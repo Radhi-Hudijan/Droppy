d12bbd19b3ce796d4c35a4caee24f35e8c7f8507
Radhi Hudijan <radhif2009@gmail.com>
2022-07-14 09:42:05 +0200

    footer are designed and fully tested and also responsive

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import Home from "./pages/Home/Home";
+
 import CreateUser from "./pages/User/CreateUser";
 import UserList from "./pages/User/UserList";
 
