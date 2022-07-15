1ae43cb7efe5e983d74623c7dd4d846b80fce5ee
Suleyman Yilmaz <yilmazsgit@gmail.com>
2022-07-15 13:08:11 +0200

    The code is cleared

--- server/src/db/connectDB.js
+++ server/src/db/connectDB.js
@@ -7,18 +7,11 @@ const connectDB = () => {
   };
   try {
     mongoose.connect(process.env.MONGODB_URL, connectionParams);
-    // console.log("Connected to database");
+    console.log("Connected to database");
   } catch (error) {
-    // console.log(error);
-    // console.log("Connection Failed");
+    console.log(error);
+    console.log("Connection Failed");
   }
-  // mongoose.connect(process.env.MONGODB_URL);
 };
 
 export default connectDB;
-
-// import mongoose from "mongoose";
-
-// const connectDB = () => mongoose.connect(process.env.MONGODB_URL);
-
-// export default connectDB;
