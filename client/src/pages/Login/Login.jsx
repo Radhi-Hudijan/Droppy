d3a42e238fe1c8823979a13873030d8c7a72cb1d
Suleyman Yilmaz <yilmazsgit@gmail.com>
2022-08-04 15:48:01 +0200

    Login page is created and styled.

---
+++ client/src/pages/Login/Login.jsx
@@ -0,0 +1,95 @@
+import React, { useEffect, useRef, useContext } from "react";
+import Button from "../../components/Button";
+import useFetch from "../../hooks/useFetch";
+import style from "./Login.module.css";
+import UserInfoContext from "../../context/UserInfoContext";
+import { Link, useNavigate } from "react-router-dom";
+import appStyles from "../../App.module.css";
+
+function Login() {
+  const navigate = useNavigate();
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+  const { setEmail, setName, setSurname, setVehicleInfo } =
+    useContext(UserInfoContext);
+
+  const onSuccess = (res) => {
+    setEmail(res.email);
+    setName(res.name);
+    setSurname(res.surname);
+    setVehicleInfo(res.vehicleInfo);
+    navigate("/", {
+      replace: true,
+    });
+  };
+
+  const { isLoading, error, performFetch, cancelFetch } = useFetch(
+    "/authentication",
+    onSuccess
+  );
+  useEffect(() => {
+    return cancelFetch;
+  }, []);
+
+  function submitHandler(e) {
+    e.preventDefault();
+    const email = emailInputRef.current.value;
+    const password = passwordInputRef.current.value;
+
+    performFetch({
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+  }
+
+  let statusComponent = null;
+  if (error != null) {
+    statusComponent = (
+      <div>Error while trying to log in: {error.toString()}</div>
+    );
+  } else if (isLoading) {
+    statusComponent = <div>Logging in....</div>;
+  }
+
+  return (
+    <div className={style.login}>
+      <h1 className={appStyles.h1Desktop}>Log in</h1>
+      <form onSubmit={submitHandler}>
+        <div>
+          <input
+            name="email"
+            required
+            ref={emailInputRef}
+            type="email"
+            aria-label="email"
+            placeholder="Email"
+            className={style.loginInput}
+          />
+        </div>
+        <div>
+          <input
+            name="password"
+            required
+            ref={passwordInputRef}
+            type="password"
+            aria-label="password"
+            placeholder="Password"
+            className={style.loginInput}
+          />
+        </div>
+        <div>
+          <Button type="submit">Log in</Button>
+        </div>
+      </form>
+      <div>
+        Do not have an account? <Link to="/user/create">Sign up here</Link>
+      </div>
+      {statusComponent}
+    </div>
+  );
+}
+
+export default Login;
