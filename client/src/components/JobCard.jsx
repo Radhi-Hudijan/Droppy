5d34fe8b6cbf19d706b86ea4e41841c9fce77b9e
Suleyman Yilmaz <yilmazsgit@gmail.com>
2022-08-10 12:46:04 +0200

    JobCard is styled

--- client/src/components/JobCard.jsx
+++ client/src/components/JobCard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { useNavigate } from "react-router-dom";
+import style from "./JobCard.module.css";
 
 function JobCard({ job }) {
   const { item, fromPostCode, toPostCode, date, width, height, length } = job;
@@ -12,23 +13,17 @@ function JobCard({ job }) {
   }
 
   return (
-    <div className="job-card" onClick={toDetail}>
+    <div className={style["job-card"]} onClick={toDetail}>
       <h3>{item}</h3>
-      <div className="from-to">
-        <div>
-          <h5>From: </h5>
-          <h5>{fromPostCode}</h5>
-        </div>
-        <div>
-          <h5>To: </h5>
-          <h5>{toPostCode}</h5>
-        </div>
+      <div className={style["from-to"]}>
+        <h5>From: {fromPostCode}</h5>
+        <h5>To: {toPostCode}</h5>
       </div>
-      <h5>{date}</h5>
-      <div className="size">
-        <h5>Width: {width}</h5>
-        <h5>Height: {height}</h5>
-        <h5>Length: {length}</h5>
+      <h5>Date: {date}</h5>
+      <div className={style["size"]}>
+        <h5>Width: {width}cm</h5>
+        <h5>Height: {height}cm</h5>
+        <h5>Length: {length}cm</h5>
       </div>
     </div>
   );
