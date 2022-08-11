67c5b283c12d911813eb67ffa37a28a313265c37
Suleyman Yilmaz <yilmazsgit@gmail.com>
2022-08-11 23:40:04 +0200

    pagination on the server side is done

--- server/src/controllers/job.js
+++ server/src/controllers/job.js
@@ -2,26 +2,27 @@ import Job, { validateJob } from "../models/Job.js";
 import User from "../models/User.js";
 import { logError } from "../util/logging.js";
 
+const ITEMS_PER_PAGE = 20;
+
 export const getAllJobs = async (req, res) => {
-  const ITEMS_PER_PAGE = 20;
   const page = req.query.page || 1;
 
   // Put all your query params in here
   const query = {};
 
+  const skip = (page - 1) * ITEMS_PER_PAGE;
   try {
-    const skip = (page - 1) * ITEMS_PER_PAGE;
-
     const count = await Job.estimatedDocumentCount(query);
 
     // await Job.deleteMany();
     const jobs = await Job.find(query).limit(ITEMS_PER_PAGE).skip(skip);
 
-    const pageCount = count / ITEMS_PER_PAGE;
+    const pageCount = Math.ceil(count / ITEMS_PER_PAGE);
 
-    res
-      .status(200)
-      .json({ success: true, result: jobs, pagination: { count, pageCount } });
+    res.status(200).json({
+      success: true,
+      result: { jobs, pagination: { count, pageCount } },
+    });
   } catch (error) {
     logError(error);
     res
@@ -30,20 +31,36 @@ export const getAllJobs = async (req, res) => {
   }
 };
 export const getActiveJobs = async (req, res) => {
+  const page = req.query.page || 1;
+
+  const skip = (page - 1) * ITEMS_PER_PAGE;
+
   try {
     let activeJobs;
     const user = await User.find({ _id: req.body.userID });
     if (!user.vehicleInfo) {
       activeJobs = await Job.find({
         senderID: req.body.userID,
-      });
+      })
+        .limit(ITEMS_PER_PAGE)
+        .skip(skip);
     } else {
       activeJobs = await Job.find({
         delivererIDs: req.body.userID,
-      });
+      })
+        .limit(ITEMS_PER_PAGE)
+        .skip(skip);
     }
 
-    res.status(200).json({ success: true, result: activeJobs });
+    const count = activeJobs.length;
+
+    const pageCount = Math.ceil(count / ITEMS_PER_PAGE);
+    console.log(pageCount, count);
+
+    res.status(200).json({
+      success: true,
+      result: { activeJobs, pagination: { count, pageCount } },
+    });
   } catch (error) {
     logError(error);
     res
