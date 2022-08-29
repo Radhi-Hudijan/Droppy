a0c492d32e6d15b7d884f22ff893c5cc14d6b703
Radhi Hudijan <radhif2009@gmail.com>
2022-08-29 13:15:29 +0200

    statistics are shown in landing page and mobile version fixd

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TEST_ID from "./Home.testid";
 
 // Style
@@ -13,8 +13,33 @@ import plus from "../../assets/icons/plus-icon.svg";
 import Button from "../../components/Button";
 import Logo from "../../components/Logo";
 import { Link } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
+import CountUp from "react-countup";
+
+import { FaUser, FaCar, FaClipboardCheck } from "react-icons/fa";
 
 const Home = () => {
+  const [totalJobAmount, setTotalJobAmount] = useState();
+  const [sendersAmount, setSendersAmount] = useState();
+  const [deliverersAmount, setDeliverersAmount] = useState();
+
+  const onSuccess = (onReceived) => {
+    setTotalJobAmount(onReceived.result.numOfTotalJobs);
+    setSendersAmount(onReceived.result.numOfSenders);
+    setDeliverersAmount(onReceived.result.numOfDeliverers);
+  };
+
+  const { performFetch, cancelFetch } = useFetch("/graphs/values", onSuccess);
+
+  useEffect(() => {
+    performFetch({
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+    return cancelFetch;
+  }, []);
   return (
     <div data-testid={TEST_ID.container}>
       <div className={style.homePage}>
@@ -47,6 +72,46 @@ const Home = () => {
             src={plus}
             text={"Add your car to your profile to sign up as a driver today!"}
           />
+        </div>
+        <div className={style.statContainer}>
+          <div className={style.row}>
+            <div className={style.col}>
+              <FaClipboardCheck className={style.statIcon} />
+              <div className={style.counter}>
+                <CountUp end={totalJobAmount} className={appStyle.h2Desktop} />
+                <div className={style.statsLine}></div>
+                <p className={`${appStyle.h2Desktop} ${style.statsText}`}>
+                  Jobs
+                </p>
+              </div>
+            </div>
+            <div className={style.col}>
+              <FaUser className={style.statIcon} />
+              <div className={style.counter}>
+                <CountUp
+                  end={sendersAmount + deliverersAmount}
+                  className={appStyle.h2Desktop}
+                />
+                <div className={style.statsLine}></div>
+                <p className={`${appStyle.h2Desktop} ${style.statsText}`}>
+                  Users
+                </p>
+              </div>
+            </div>
+            <div className={style.col}>
+              <FaCar className={style.statIcon} />
+              <div className={style.counter}>
+                <CountUp
+                  end={deliverersAmount}
+                  className={appStyle.h2Desktop}
+                />
+                <div className={style.statsLine}></div>
+                <p className={`${appStyle.h2Desktop} ${style.statsText}`}>
+                  Drivers
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
