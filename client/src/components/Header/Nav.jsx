eaae8c12015a42144d6284a6e1aa649b68a2d0c1
Edward Abboud <abboudedward@gmail.com>
2022-08-29 14:28:47 +0200

    added profile tab to navbar

--- client/src/components/Header/Nav.jsx
+++ client/src/components/Header/Nav.jsx
@@ -6,7 +6,6 @@ import useMediaQuery, { QUERIES } from "../../hooks/useMediaQuery";
 import Logo from "../Logo";
 import styles from "./Nav.module.css";
 import appStyle from "../../App.module.css";
-import TEST_ID from "./Nav.testid";
 import { motion } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Divider from "../Divider";
@@ -15,13 +14,9 @@ import { useState } from "react";
 import burger from "../../assets/icons/burger-icon.svg";
 import UserInfoContext from "../../context/UserInfoContext";
 
-const NAV_CONTENT = [
-  { link: "/about", id: TEST_ID.linkToAbout, value: "About" },
-];
 const Nav = ({ opened }) => {
   const isMdScreen = useMediaQuery(QUERIES.md);
   const [isOpen, setIsOpen] = useState(false);
-
   const openHandler = () => {
     setIsOpen(!isOpen);
     opened();
@@ -59,11 +54,33 @@ const Nav = ({ opened }) => {
             </li>
           </Link>
         )}
-        {NAV_CONTENT.map((x, idx) => (
+        <Link
+          key={1}
+          to={"/about"}
+          data-testid="linkToAbout"
+          onClick={() => {
+            if (isMdScreen) return;
+            setIsOpen(false);
+            opened();
+          }}
+        >
+          <li className={appStyle.h2Desktop}>
+            <motion.div
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              style={{ flexDirection: "row" }}
+            >
+              <span>About</span>
+              {!isMdScreen && <FontAwesomeIcon icon={faArrowRight} />}
+            </motion.div>
+          </li>
+        </Link>
+
+        {token && (
           <Link
-            key={idx}
-            to={x.link}
-            data-testid={x.id}
+            key="3"
+            to={`/profile/${localStorage.getItem("userID")}`}
+            data-testid="linkToLogin"
             onClick={() => {
               if (isMdScreen) return;
               setIsOpen(false);
@@ -76,14 +93,14 @@ const Nav = ({ opened }) => {
                 whileTap={{ scale: 0.9 }}
                 style={{ flexDirection: "row" }}
               >
-                <span>{x.value}</span>
+                <span>Profile</span>
                 {!isMdScreen && <FontAwesomeIcon icon={faArrowRight} />}
               </motion.div>
             </li>
           </Link>
-        ))}
+        )}
         <Link
-          key="3"
+          key="4"
           to={token ? "/" : "/login"}
           data-testid="linkToLogin"
           onClick={() => {
